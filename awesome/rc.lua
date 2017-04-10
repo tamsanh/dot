f3b2a4d01d3f382a1f7f61d2f8b7b17c51ba2eb9
Tam Nguyen <tamsanh@gmail.com>
2017-04-11 04:59:30 +0900

    Remove floating

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -67,11 +67,11 @@ local layouts =
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.magnifier,
-    awful.layout.suit.floating
 }
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.max.fullscreen,
     -- awful.layout.suit.max,
+    -- awful.layout.suit.floating
 -- }}}
 
 -- {{{ Wallpaper
