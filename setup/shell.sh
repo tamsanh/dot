629179db6978d17d8f006eeb22bff61b1f27f0fc
Tam Nguyen <tamsanh@gmail.com>
2023-06-25 17:46:15 -1000

    Add auto-update based on last_check time

--- setup/shell.sh
+++ setup/shell.sh
@@ -40,14 +40,28 @@ function add_shellrc {
 	cat <(cat ${rc_head}) <(
 		cat <<EOF
 ${start_header}
+
 source ${ROOT_DIR}/shellrc
+
+if [ ! -e ${ROOT_DIR}/last_check]; then
+  date +%s > ${ROOT_DIR}/last_check
+fi
+
+last_check_time=\$(cat \${ROOT_DIR}/last_check)
+curr_time=\$(date +%s)
+
+time_since_last_check=\$((\$(curr_time)-\$(last_check_time)))
+if [ \${time_since_last_check} -ge 86400 ]; then
+  git -C ${ROOT_DIR} pull origin main > /dev/null
+  date +%s > ${ROOT_DIR}/last_check
+fi
+
 ${stop_header}
 EOF) <(cat ${rc_tail}) > ${new_rc}
 
 	# Overwrite the previous rc
   cp ${rc}{,.bak}
   mv ${new_rc} ${rc}
-
 }
 
 # Adds to .bashrc
