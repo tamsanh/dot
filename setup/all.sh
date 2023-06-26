c4ff1100085d0903fb32a85d748d845150fae4a0
Tam Nguyen <tamsanh@gmail.com>
2023-06-25 17:18:03 -1000

    Add initial setup commands

---
+++ setup/all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CURR_DIR=$(
+	cd $(dirname $0)
+	pwd
+)
+cd $CURR_DIR
+
+./shell.sh
+./nvim.sh
---
+++ setup/shell.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -x
+
+CURR_DIR=$(
+	cd $(dirname $0)
+	pwd
+)
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+cd ${ROOT_DIR}
+
+function add_shellrc {
+	rc=$1
+
+	# Preserve original rc
+	start_header=\#\ ======\ START\ tamsanh-dot\ ======
+	stop_header=\#\ ======\ END\ tamsanh-dot\ ======
+
+	rc_head=$(mktemp)
+	rc_tail=$(mktemp)
+
+	# Find the before and after of the header
+	fgrep "${start_header}" ${rc} >/dev/null
+	if [ $? -ne 0 ]; then
+		cat ${rc} >${rc_head}
+	else
+		start_loc=$(awk '/'${start_header}'/ {print FNR}' ${rc})
+		end_loc=$(awk '/'${stop_header}'/ {print FNR}' ${rc})
+		remaining_count=$(wc -l ${rc})
+		head -n ${start_dot} >${rc_head}
+		tail -n $((${line_count} - ${end_dot})) >${rc_tail}
+	fi
+
+	# Add reference
+	new_rc=$(mktemp)
+	cat <(cat ${rc_head}) <(
+		cat <<EOF
+${start_header}
+source ${ROOT_DIR}/shellrc
+${stop_header}
+EOF) <(cat ${rc_tail}) > ${new_rc}
+
+	# Overwrite the previous rc
+  cp ${rc}{,.bak}
+  mv ${new_rc} ${rc}
+
+}
+
+# Adds to .bashrc
+if [ -e ${HOME}/.bashrc ]; then
+  add_shellrc ${HOME}/.bashrc
+fi
+
+# Adds to .zprofile
+if [ -e ${HOME}/.zprofile ]; then
+  add_shellrc ${HOME}/.zprofile
+fi
