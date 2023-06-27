99ab5496f47f15fbc7c049dc3c451220cf28a7ff
Tam Nguyen <tamsanh@gmail.com>
2023-06-26 14:04:22 -1000

    Enable leap and disable leap's s key

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,1 +1,9 @@
-return { "ggandor/leap.nvim", enabled = false }
+-- Override leap to remove the 's' key
+-- We need to keep leap so that 'flit' can still work
+return {
+  "ggandor/leap.nvim",
+  config = function(_, _)
+    vim.keymap.del({ "n", "x", "o" }, "s")
+    vim.keymap.del({ "n", "x", "o" }, "S")
+  end,
+}
