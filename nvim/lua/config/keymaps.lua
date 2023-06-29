c0bc179076b4230876e8043c64ede008950fe5e1
Tam Nguyen <tamsanh@gmail.com>
2023-06-29 07:42:12 -1000

    Remove the telescope resume keys

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
 vim.keymap.set("n", "<C-/>", "<cmd>TestFile -strategy=toggleterm<cr>", { silent = true })
