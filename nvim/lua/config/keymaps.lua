cc9c5f55fd2b6b3ed17aafd477cffbf3f13323a9
Tam Nguyen <tamsanh@gmail.com>
2023-06-30 09:56:46 -1000

    Use C-d for test running

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-/>", "<cmd>TestFile -strategy=toggleterm<cr>", { silent = true })
+vim.keymap.set("n", "<C-d>", "<cmd>TestFile -strategy=toggleterm<cr>", { silent = true })
