cd453a34455c5fc19346189bad0c3155ce1ed502
Tam Nguyen <tamsanh@gmail.com>
2026-04-01 10:15:21 -0400

    Do not open neotree on enter

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -6,14 +6,5 @@ return {
         hijack_netrw_behavior = "open_current",
       },
     },
-    init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          if vim.fn.argc() == 0 then
-            require("neo-tree.command").execute({ action = "show" })
-          end
-        end,
-      })
-    end,
   },
 }
